c4f70a593a811c0eac662eb38b31a6de36469aed
vladkryv <83036866+vladkryv@users.noreply.github.com>
2024-08-03 14:47:38 +0300

    Set additional header and update UA
    Currently, when 'set-ch-ua' is present in a request, bing considers it a higher priority than 'user-agent'. This also works if you "remove" this header rather than "set" it. The 'sec-ch-ua-full-version-list' is not necessarily be set/remove, it is present in requests, but is ignored for now.

--- package-brave/background.js
+++ package-brave/background.js
@@ -1,8 +1,11 @@
 chrome.webRequest.onBeforeSendHeaders.addListener(data => {
 	for (let header of data.requestHeaders) {
-		if (header.name.toLowerCase() === 'user-agent') {
-			header.value = navigator.userAgent.split('AppleWebKit')[0] + 'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36 Edg/124.0.0.0';
-		}
+		let currentHeader = header.name.toLowerCase();
+	        if (currentHeader === 'user-agent') {
+	            header.value = navigator.userAgent.split('AppleWebKit')[0] + 'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36 Edg/127.0.0.0';
+	        } else if (currentHeader === 'sec-ch-ua') {
+	            header.value = '"Not)A;Brand";v="99", "Microsoft Edge";v="127", "Chromium";v="127"';
+	        }
 	}
 	return { requestHeaders: data.requestHeaders };
 }, { urls: ['https://*.bing.com/*', 'https://copilot.microsoft.com/*'] }, ['blocking', 'requestHeaders']);
